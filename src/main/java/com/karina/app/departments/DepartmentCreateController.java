46ff3323bbae10a0eedd8c2b4d76fbd45847fb53
Jonghyun <zxc159285@gmail.com>
2026-03-26 13:19:27 +0900

    0326-1

--- src/main/java/com/karina/app/departments/DepartmentCreateController.java
+++ src/main/java/com/karina/app/departments/DepartmentCreateController.java
@@ -38,7 +38,7 @@ public class DepartmentCreateController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println("DB에 저장해야돼");
 	}
 
 }
